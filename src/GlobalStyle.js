eeff853ba38508aeeb2fead58cdb26ad1c53334a
W Souza <dev9.ws@gmail.com>
2024-04-28 23:34:18 -0300

    feature/font

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -5,6 +5,10 @@ export const GlobalStyle = createGlobalStyle`
         margin: 0;
         padding: 0;
         box-sizing: border-box;
+
+        font-family: "Roboto", sans-serif;
+
+        letter-spacing: 0.7px;
     }
 
     a{
